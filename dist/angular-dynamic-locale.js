6f84888b4ccf1e5f630d0356a42114c61b0ea543
Carey Hinoki <carey@shift.com>
2014-02-19 20:56:49 -0800

    chore(locale): Prototyping.

--- dist/angular-dynamic-locale.js
+++ dist/angular-dynamic-locale.js
@@ -75,71 +75,67 @@
   @todo Add configurability since it is a provider
    */
 
-  dl.provider("dlProvider", [
-    "dlValue", function(dlValue) {
-      var currencyMap, defaultLocale, getModuleName, locales, ngLocaleMap;
-      ngLocaleMap = {};
-      defaultLocale = 'en-us';
-      currencyMap = {
-        "USD": "en-us"
-      };
-      locales = ['en-us'];
-      getModuleName = function(locale) {
-        return "dynamic.locale." + locale;
-      };
-      this.setCurrencyMap = function(map) {
-        return currencyMap = map;
-      };
-      this.setDefaultLocale = function(locale) {
-        return defaultLocale = locale;
-      };
-      this.setLocales = function(locales) {
-        return locales = locales;
-      };
-      this.loadLocales = function(callback) {
-        angular.forEach(locales, (function(_this) {
-          return function(locale, key) {
-            return _this.loadLocale(locale);
-          };
-        })(this));
-        if (dlValue[defaultLocale] != null) {
-          eval(dlValue[defaultLocale]);
-        } else {
-          throw Error("defaultLocale must be in locales");
+  dl.provider("dlProvider", function() {
+    var currencyMap, defaultLocale, getModuleName, locales, ngLocaleMap;
+    ngLocaleMap = {};
+    defaultLocale = 'en-us';
+    currencyMap = {
+      "USD": "en-us"
+    };
+    locales = ['en-us'];
+    getModuleName = function(locale) {
+      return "dynamic.locale." + locale;
+    };
+    this.setCurrencyMap = function(map) {
+      return currencyMap = map;
+    };
+    this.setDefaultLocale = function(locale) {
+      return defaultLocale = locale;
+    };
+    this.setLocales = function(locales) {
+      return locales = locales;
+    };
+    this.loadLocales = function(callback) {
+      angular.forEach(locales, (function(_this) {
+        return function(locale, key) {
+          return _this.loadLocale(locale);
+        };
+      })(this));
+      if (dlValue[defaultLocale] != null) {
+        eval(dlValue[defaultLocale]);
+      } else {
+        throw Error("defaultLocale must be in locales");
+      }
+      return callback.apply();
+    };
+    this.loadLocale = function(locale) {
+      var module;
+      eval(dlValue[locale]);
+      module = angular.module(getModuleName(locale), []);
+      return module._invokeQueue.push(ngLocaleModule._invokeQueue[0]);
+    };
+    this.$get = function() {
+      return {
+        currencyFilters: {},
+        dateFilters: {},
+        loadCurrencyFilters: function() {
+          return angular.forEach(locales, (function(_this) {
+            return function(locale, key) {
+              return _this.currencyFilters[locale] = angular.injector(["ng", getModuleName(locale)]).get("$filter")("currency");
+            };
+          })(this));
+        },
+        loadDateFilters: function() {
+          return angular.forEach(locales, (function(_this) {
+            return function(locale, key) {
+              return _this.dateFilters[locale] = angular.injector(["ng", getModuleName(locale)]).get("$filter")("date");
+            };
+          })(this));
         }
-        return callback.apply();
       };
-      this.loadLocale = function(locale) {
-        var module;
-        eval(dlValue[locale]);
-        module = angular.module(getModuleName(locale), []);
-        return module._invokeQueue.push(ngLocaleModule._invokeQueue[0]);
-      };
-      this.$get = [
-        "$q", function($q) {
-          return {
-            currencyFilters: {},
-            dateFilters: {},
-            loadCurrencyFilters: function() {
-              return angular.forEach(locales, (function(_this) {
-                return function(locale, key) {
-                  return _this.currencyFilters[locale] = angular.injector(["ng", getModuleName(locale)]).get("$filter")("currency");
-                };
-              })(this));
-            },
-            loadDateFilters: function() {
-              return angular.forEach(locales, (function(_this) {
-                return function(locale, key) {
-                  return _this.dateFilters[locale] = angular.injector(["ng", getModuleName(locale)]).get("$filter")("date");
-                };
-              })(this));
-            }
-          };
-        }
-      ];
-      return this;
-    }
-  ]);
+    };
+    return this;
+  });
 
   dl.value("dlValue", {
     "de": "'use strict';\nangular.module(\"ngLocale\", [], [\"$provide\", function($provide) {\nvar PLURAL_CATEGORY = {ZERO: \"zero\", ONE: \"one\", TWO: \"two\", FEW: \"few\", MANY: \"many\", OTHER: \"other\"};\n$provide.value(\"$locale\", {\n  \"DATETIME_FORMATS\": {\n    \"AMPMS\": [\n      \"vorm.\",\n      \"nachm.\"\n    ],\n    \"DAY\": [\n      \"Sonntag\",\n      \"Montag\",\n      \"Dienstag\",\n      \"Mittwoch\",\n      \"Donnerstag\",\n      \"Freitag\",\n      \"Samstag\"\n    ],\n    \"MONTH\": [\n      \"Januar\",\n      \"Februar\",\n      \"M\u00e4rz\",\n      \"April\",\n      \"Mai\",\n      \"Juni\",\n      \"Juli\",\n      \"August\",\n      \"September\",\n      \"Oktober\",\n      \"November\",\n      \"Dezember\"\n    ],\n    \"SHORTDAY\": [\n      \"So.\",\n      \"Mo.\",\n      \"Di.\",\n      \"Mi.\",\n      \"Do.\",\n      \"Fr.\",\n      \"Sa.\"\n    ],\n    \"SHORTMONTH\": [\n      \"Jan\",\n      \"Feb\",\n      \"M\u00e4r\",\n      \"Apr\",\n      \"Mai\",\n      \"Jun\",\n      \"Jul\",\n      \"Aug\",\n      \"Sep\",\n      \"Okt\",\n      \"Nov\",\n      \"Dez\"\n    ],\n    \"fullDate\": \"EEEE, d. MMMM y\",\n    \"longDate\": \"d. MMMM y\",\n    \"medium\": \"dd.MM.yyyy HH:mm:ss\",\n    \"mediumDate\": \"dd.MM.yyyy\",\n    \"mediumTime\": \"HH:mm:ss\",\n    \"short\": \"dd.MM.yy HH:mm\",\n    \"shortDate\": \"dd.MM.yy\",\n    \"shortTime\": \"HH:mm\"\n  },\n  \"NUMBER_FORMATS\": {\n    \"CURRENCY_SYM\": \"\u20ac\",\n    \"DECIMAL_SEP\": \",\",\n    \"GROUP_SEP\": \".\",\n    \"PATTERNS\": [\n      {\n        \"gSize\": 3,\n        \"lgSize\": 3,\n        \"macFrac\": 0,\n        \"maxFrac\": 3,\n        \"minFrac\": 0,\n        \"minInt\": 1,\n        \"negPre\": \"-\",\n        \"negSuf\": \"\",\n        \"posPre\": \"\",\n        \"posSuf\": \"\"\n      },\n      {\n        \"gSize\": 3,\n        \"lgSize\": 3,\n        \"macFrac\": 0,\n        \"maxFrac\": 2,\n        \"minFrac\": 2,\n        \"minInt\": 1,\n        \"negPre\": \"-\",\n        \"negSuf\": \"\u00a0\u00a4\",\n        \"posPre\": \"\",\n        \"posSuf\": \"\u00a0\u00a4\"\n      }\n    ]\n  },\n  \"id\": \"de\",\n  \"pluralCat\": function (n) {  if (n == 1) {   return PLURAL_CATEGORY.ONE;  }  return PLURAL_CATEGORY.OTHER;}\n});\n}]);",
--- src/dl-provider.coffee
+++ src/dl-provider.coffee
@@ -8,70 +8,60 @@
 
 ###
 
-dl.provider "dlProvider", [
-  "dlValue"
-  (
-    dlValue
-  ) ->
-    ngLocaleMap = {}
+dl.provider "dlProvider", ->
+  ngLocaleMap = {}
 
-    defaultLocale = 'en-us'
+  defaultLocale = 'en-us'
 
-    currencyMap =
-      "USD": "en-us"
+  currencyMap =
+    "USD": "en-us"
 
-    locales = [
-      'en-us'
-    ]
+  locales = [
+    'en-us'
+  ]
 
-    getModuleName = (locale) ->
-      return "dynamic.locale.#{locale}"
+  getModuleName = (locale) ->
+    return "dynamic.locale.#{locale}"
 
-    @setCurrencyMap = (map) ->
-      currencyMap = map
+  @setCurrencyMap = (map) ->
+    currencyMap = map
 
-    @setDefaultLocale = (locale) ->
-      defaultLocale = locale
+  @setDefaultLocale = (locale) ->
+    defaultLocale = locale
 
-    @setLocales = (locales) ->
-      locales = locales
+  @setLocales = (locales) ->
+    locales = locales
 
-    @loadLocales = (callback) ->
-      angular.forEach locales, (locale, key) =>
-        @loadLocale locale
+  @loadLocales = (callback) ->
+    angular.forEach locales, (locale, key) =>
+      @loadLocale locale
 
-      # set default locale
-      if dlValue[defaultLocale]?
-        eval dlValue[defaultLocale]
-      else
-        throw Error "defaultLocale must be in locales"
+    # set default locale
+    if dlValue[defaultLocale]?
+      eval dlValue[defaultLocale]
+    else
+      throw Error "defaultLocale must be in locales"
 
-      callback.apply()
+    callback.apply()
 
-    @loadLocale = (locale) ->
-      # set ngLocale
-      eval dlValue[locale]
+  @loadLocale = (locale) ->
+    # set ngLocale
+    eval dlValue[locale]
 
-      # create new module for locale
-      module = angular.module(getModuleName(locale), [])
-      module._invokeQueue.push ngLocaleModule._invokeQueue[0]
+    # create new module for locale
+    module = angular.module(getModuleName(locale), [])
+    module._invokeQueue.push ngLocaleModule._invokeQueue[0]
 
-    @$get = [
-      "$q"
-      (
-        $q
-      ) ->
-        currencyFilters: {}
-        dateFilters:     {}
+  @$get = ->
+      currencyFilters: {}
+      dateFilters:     {}
 
-        loadCurrencyFilters: ->
-          angular.forEach locales, (locale, key) =>
-            @currencyFilters[locale] = angular.injector(["ng", getModuleName(locale)]).get("$filter")("currency")
+      loadCurrencyFilters: ->
+        angular.forEach locales, (locale, key) =>
+          @currencyFilters[locale] = angular.injector(["ng", getModuleName(locale)]).get("$filter")("currency")
 
-        loadDateFilters: ->
-          angular.forEach locales, (locale, key) =>
-            @dateFilters[locale] = angular.injector(["ng", getModuleName(locale)]).get("$filter")("date")
-    ]
+      loadDateFilters: ->
+        angular.forEach locales, (locale, key) =>
+          @dateFilters[locale] = angular.injector(["ng", getModuleName(locale)]).get("$filter")("date")
 
-    return @
-]
+  return @
